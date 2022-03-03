39421fa8752b1299e38e731f667b354a8a67cec2
John Alexis Guerra Gómez <john.guerra@gmail.com>
2022-03-02 21:33:46 -0800

    Adding a controlled component

--- src/components/PublicationsGallery.js
+++ src/components/PublicationsGallery.js
@@ -1,23 +1,73 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-
 import PublicationView from "./PublicationView.js";
 
 
+/**
+ * Just to demonstrate a controlled component
+ */
+class SearchComponent extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+        <label className="form-label mb-2">
+          Search:
+          <input
+            type="text"
+            value={this.props.searchQuery}
+            onChange={(evt) => this.props.onChangeSearchQuery(evt.target.value)}
+            className="form-control"
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
 export class PublicationsGallery extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchQuery: "",
+    };
+  }
+
   /**
    * A helper method to render just the publications.
    * @return {JSX} the element containing the publications
    */
   renderPublications() {
-    return this.props.publications.map((p, i) => (
-      <PublicationView key={"pub"+i} pub={p}></PublicationView>
-    ));
+    return this.props.publications
+      .filter((p) => p.title.indexOf(this.state.searchQuery) !== -1)
+      .slice(0, 20)
+      .map((p, i) => (
+        <PublicationView key={"pub" + i} pub={p}></PublicationView>
+      ));
+  }
+
+  onChangeSearchQuery(_newQuery) {
+    this.setState({
+      searchQuery: _newQuery,
+    });
   }
 
   render() {
-    return <div className="row">{this.renderPublications()}</div>;
+    return (
+      <div className="row">
+        <SearchComponent
+          searchQuery={this.state.searchQuery}
+          onChangeSearchQuery={this.onChangeSearchQuery.bind(this)}
+        ></SearchComponent>
+
+        <div className="mb-3">
+          You searched for: <output>{this.state.searchQuery}</output>
+        </div>
+        {this.renderPublications()}
+      </div>
+    );
   }
 }
 
